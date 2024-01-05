f57938fb8be989dcc64e6262f90f64091d5410ad
Вячеслав <slavyan06081997@gmail.com>
2024-01-05 17:34:54 +0300

    add prefab food

--- Assets/Scripts/SelectedCounterVisual.cs
+++ Assets/Scripts/SelectedCounterVisual.cs
@@ -6,11 +6,25 @@ public class SelectedCounterVisual : MonoBehaviour {
     
 
     [SerializeField] private ClearCounter clearCounter;
+    [SerializeField] private GameObject visualGameObject;
     private void Start() {
        Player.Instance.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
     }
 
     private void Player_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e) {
-        //e.selectedCounter
+        if (e.selectedCounter == clearCounter) {
+            Show();
+        } else{
+            Hide();
+        }
+    }
+     private void Show() {
+        visualGameObject.SetActive(true);
+     }
+
+     private void Hide() {
+        visualGameObject.SetActive(false);
     }
 }
+
+    
