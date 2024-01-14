e3fb6044f31734541a5bae05d395960004c38b4a
Вячеслав <slavyan06081997@gmail.com>
2024-01-14 18:55:45 +0300

    interface

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour {
+public class ClearCounter : MonoBehaviour, IKitchenObjectParent {
 
    [SerializeField] private KitchenObjectSO kitchenObjectSO;
    [SerializeField] private Transform counterTopPoint;
@@ -14,7 +14,7 @@ public class ClearCounter : MonoBehaviour {
    private void Update() {
       if (testing && Input.GetKeyDown(KeyCode.T)) {
          if (kitchenObject != null) {
-            kitchenObject.SetClearCounter(secondClearCounter);
+            kitchenObject.SetKitchenObjectParent(secondClearCounter);
          }
          
       }
@@ -22,13 +22,15 @@ public class ClearCounter : MonoBehaviour {
    }
    
    
-   public void Interact() {
+   public void Interact(Player player) {
       if (kitchenObject == null) {
          Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-         kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+         kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
         
       }  else {
-           Debug.Log(kitchenObject.GetClearCounter());
+         // Give the object to player
+         kitchenObject.SetKitchenObjectParent(player);
+           
       }  
    }
    
@@ -51,5 +53,5 @@ public class ClearCounter : MonoBehaviour {
 
    public bool HasKitchenObject() {
       return kitchenObject != null;
-   }
+   } 
 }
