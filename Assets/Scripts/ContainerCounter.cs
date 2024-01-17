0fb5d3f692c030feba82f52d6e534aaf8524e00b
Вячеслав <slavyan06081997@gmail.com>
2024-01-17 18:19:41 +0300

    refactoring

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -2,46 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : BaseCounter, IKitchenObjectParent {
+public class ContainerCounter : BaseCounter, IKitchenObjectParent {
+    
 
-   [SerializeField] private KitchenObjectSO kitchenObjectSO;
-   [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
 
-   private KitchenObject kitchenObject;
-
-   
-   
-   
-   public override void Interact(Player player) {
+    private KitchenObject kitchenObject;
+    
+    public override void Interact(Player player) {
       if (kitchenObject == null) {
-         Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-         kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
         
-      }  else {
+      } else {
          // Give the object to player
          kitchenObject.SetKitchenObjectParent(player);
-           
-      }  
-   }
-   
+        
+        }  
 
+    } 
+   
    public Transform GetKitchenObjectFollowTransform() {
       return counterTopPoint;
-   }
+    }
 
    public void SetKitchenObject(KitchenObject kitchenObject) {
       this.kitchenObject = kitchenObject;
-   }
+    }
 
    public KitchenObject GetKitchenObject() {
       return kitchenObject;
-   }
+    }
 
    public void ClearKitchenObject() {
       kitchenObject = null;
-   }
+    }
 
    public bool HasKitchenObject() {
       return kitchenObject != null;
-   } 
+    } 
+   
 }
